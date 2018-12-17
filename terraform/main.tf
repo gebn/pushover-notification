d13943092b663b12264d34673ce05a18ff34d327
George Brighton <george@gebn.co.uk>
2018-12-17 00:34:23 +0000

    Make eu-west-1 the primary provider region for consistency with state storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,26 +11,17 @@ provider "aws" {
   version    = "~> 1.52"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "us-east-1"
+  region     = "eu-west-1"
 }
 
 provider "aws" {
-  alias  = "ew1"
-  region = "eu-west-1"
+  alias  = "ue1"
+  region = "us-east-1"
 }
 
 provider "aws" {
   alias  = "ew2"
   region = "eu-west-2"
-}
-
-module "deploy_ue1" {
-  source                     = "./pushover_notification"
-  sns_access_account_ids     = ["${var.skylab_account_id}"]
-  deployment_package         = "${var.deployment_package}"
-  publish_function           = "${var.publish_function}"
-  default_pushover_app_token = "${var.default_pushover_app_token}"
-  default_pushover_user_key  = "${var.default_pushover_user_key}"
 }
 
 module "deploy_ew1" {
@@ -40,9 +31,18 @@ module "deploy_ew1" {
   publish_function           = "${var.publish_function}"
   default_pushover_app_token = "${var.default_pushover_app_token}"
   default_pushover_user_key  = "${var.default_pushover_user_key}"
+}
+
+module "deploy_ue1" {
+  source                     = "./pushover_notification"
+  sns_access_account_ids     = ["${var.skylab_account_id}"]
+  deployment_package         = "${var.deployment_package}"
+  publish_function           = "${var.publish_function}"
+  default_pushover_app_token = "${var.default_pushover_app_token}"
+  default_pushover_user_key  = "${var.default_pushover_user_key}"
 
   providers = {
-    aws = "aws.ew1"
+    aws = "aws.ue1"
   }
 }
 
