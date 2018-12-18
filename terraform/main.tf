8b831d19d9f8f0694f0e317b7d40017551617422
George Brighton <george@gebn.co.uk>
2018-12-18 18:54:23 +0000

    Revert "Only deploy to eu-west-1 to start with"
    This reverts commit e649811c9765babd4db1c8fb94b28a5b666c645d.

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,29 @@ module "deploy_ew1" {
   default_pushover_app_token = "${var.default_pushover_app_token}"
   default_pushover_user_key  = "${var.default_pushover_user_key}"
 }
+
+module "deploy_ue1" {
+  source                     = "./pushover_notification"
+  sns_access_account_ids     = ["${var.skylab_account_id}"]
+  deployment_package         = "${var.deployment_package}"
+  publish_function           = "${var.publish_function}"
+  default_pushover_app_token = "${var.default_pushover_app_token}"
+  default_pushover_user_key  = "${var.default_pushover_user_key}"
+
+  providers = {
+    aws = "aws.ue1"
+  }
+}
+
+module "deploy_ew2" {
+  source                     = "./pushover_notification"
+  sns_access_account_ids     = ["${var.skylab_account_id}"]
+  deployment_package         = "${var.deployment_package}"
+  publish_function           = "${var.publish_function}"
+  default_pushover_app_token = "${var.default_pushover_app_token}"
+  default_pushover_user_key  = "${var.default_pushover_user_key}"
+
+  providers = {
+    aws = "aws.ew2"
+  }
+}
