7a1c1189c32c43549993baf58933e94cf21220b4
George Brighton <george@gebn.co.uk>
2018-12-18 17:32:55 +0000

    Terraform Travis config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,7 @@
 variable "skylab_account_id" {}
 variable "deployment_package" {}
-variable "publish_function" {}
+variable "publish_function" {
+    default = false
+}
 variable "default_pushover_app_token" {}
 variable "default_pushover_user_key" {}
