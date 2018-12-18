06fdd05127ddeafa6780cc92d74170d188f8df89
George Brighton <george@gebn.co.uk>
2018-12-18 18:24:41 +0000

    terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,9 @@
 variable "skylab_account_id" {}
 variable "deployment_package" {}
+
 variable "publish_function" {
-    default = false
+  default = false
 }
+
 variable "default_pushover_app_token" {}
 variable "default_pushover_user_key" {}
